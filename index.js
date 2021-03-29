a7ac5c7836040d614094c586a4a6e43af5974c57
Caue Massi <cauemassi@gmail.com>
2021-03-28 23:00:54 -0300

    adjust index.js - include body parser

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require('./src/models/index')
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const routes = require('./src/routes/api');
 
 app.get('/', function(req, res){
   res.send('END POINT INVÁLIDO!');
 });
 
+app.use(bodyParser.json());
 app.use('/api', routes);
 
-app.listen(process.env.PORT || 3000); // faz com que o servidor seja executado na porta 3000 do seu localhost:3000
+app.listen(process.env.PORT || 3000)
