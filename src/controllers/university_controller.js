4c7508b1844c2feef1d71049712786c028d1273f
Caue Massi <cauemassi@gmail.com>
2021-03-28 23:04:59 -0300

    finish crud api universities

--- src/controllers/university_controller.js
+++ src/controllers/university_controller.js
@@ -1,25 +1,68 @@
-exports.index = function (request, response) {
-  University.findAll()
-    .then(data => {
-      response.status(200).send(data);
-    })
-    .catch(error =>
-      response.status(500).send({message: error.message || 'Some error occurred'}));
+exports.index = async function (request, response) {
+  try {
+    universities = await University.findAll();
+    
+    return response.status(200).send(universities);
+  } catch(error){
+    return response.status(400).send({
+      message: errors.errors.map((error) => error.message) 
+      || 'Some error occurred'
+    });
+  }
 };
 
-exports.show = function (request, response) {
-  response.send('/GET');
+exports.show = async function (request, response) {
+  try {
+    university = await University.findByPk(request.params.id);
+    
+    return response.status(200).send(university);
+  } catch(error){
+    return response.status(400).send({
+      message: errors.errors.map((error) => error.message) 
+      || 'Some error occurred'
+    });
+  }
 };
 
-exports.create = function (request, response) {
-  response.send('/POST');
+exports.create = async function (request, response) {
+  try {
+    university = await University.create(request.body);
+    
+    return response.status(200).send(university);
+  } catch(errors){
+    return response.status(400).send({
+      message: errors.errors.map((error) => error.message) 
+      || 'Some error occurred'
+    });
+  }
 };
 
-exports.update = function (request, response) {
-  response.send('/PUT');
+exports.update = async function (request, response) {
+  try {
+    university = await University.findByPk(request.params.id);
+
+    university.update(request.body);
+    
+    return response.status(200).send(university);
+  } catch(error){
+    return response.status(400).send({
+      message: errors.errors.map((error)=> error.message) 
+      || 'Some error occurred'
+    });
+  }
 };
 
-exports.delete = function (request, response) {
-  response.send('/DELETE');
-};
+exports.delete = async function (request, response) {
+  try {
+    university = await University.findByPk(request.params.id);
 
+    university.destroy();
+
+    return response.status(200).send(request.json());
+  } catch(error){
+    return response.status(400).send({
+      message: errors.errors.map((error)=> error.message) 
+      || 'Some error occurred'
+    });
+  }
+};
